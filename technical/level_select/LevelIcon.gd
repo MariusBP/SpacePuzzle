c5d723383c13376e15f85df69ec1775dafef7b21
Marius Pran <mariusbpran@gmail.com>
2022-03-29 14:03:39 +0200

    Added code to LevelIcon
    continue as https://kidscancode.org/godot_recipes/ui/level_select/

--- technical/level_select/LevelIcon.gd
+++ technical/level_select/LevelIcon.gd
@@ -2,16 +2,26 @@ extends PanelContainer
 
 signal level_selected
 
+onready var Number = $Number
+onready var Lock = $MarginContainer
+
 var locked = true setget set_locked
 var level_num = 1 setget set_level
 
 
 
 func set_locked(value):
-	pass
+	locked = value
+	Lock.visible = value
+	Number.visible = !value
 
 func set_level(value):
-	pass
+	level_num = value
+	Number.text = str(level_num)
 
 func _on_LevelIcon_gui_input(event):
-	pass # Replace with function body.
+	if locked:
+		return
+	if event is InputEventMouseButton and event.pressed:
+		print("Clicked Level:", level_num)
+		emit_signal("level_selected", level_num)
